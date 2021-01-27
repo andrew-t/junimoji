e19fc589d8cc25dbbbcbf2079b3efc90f9c064d1
Andrew <andrew@andrewt.net>
2021-01-27 22:36:15 +0000

    Redesigned sidebar

--- collapse-sidebar.js
+++ collapse-sidebar.js
@@ -1,5 +1,16 @@
+import { clearClass } from './dom-tools.js';
+
 const button = document.getElementById('toggle-sidebar'),
 	sidebar = document.getElementById('sidebar');
 
 button.addEventListener('click',
 	e => sidebar.classList.toggle('collapsed'));
+
+for (const tab of document.querySelectorAll('.tab')) {
+	const toggle = tab.querySelector('.toggle');
+	toggle.addEventListener('click', e => {
+		clearClass('expanded');
+		tab.scrollTo(0, 0);
+		tab.classList.add('expanded');
+	});
+}
